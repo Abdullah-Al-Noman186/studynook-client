b51e684b96fa46928a1ddd49b2a0a398e060d5e0
unknown <you@example.com>
2026-06-11 17:13:58 +0600

    fix useEffect import in login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState } from "react";
+
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FcGoogle } from "react-icons/fc";
 import { authClient } from "@/lib/auth-client";
+import { useState, useEffect } from "react";
 
 const LoginPage = () => {
   const router = useRouter();
